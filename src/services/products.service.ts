e23655f2fb206980df865aec6abd6dc8aca62bd4
AugustoMagalhaes <augustommg@gmail.com>
2022-08-01 18:29:12 -0300

    feat: implementa servive ProductService.getAllProducts.

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -9,6 +9,11 @@ export default class ProductService {
     this.model = new ProductModel(connection);
   }
 
+  public getAllProducts = async (): Promise<Product[]> => {
+    const products = await this.model.getAllProducts();
+    return products;
+  };
+
   public createProduct = async (product: Product): Promise<Product> => {
     const createdProduct = await this.model.createProduct(product);
     return createdProduct;
