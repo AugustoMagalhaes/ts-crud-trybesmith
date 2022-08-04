d3b4886d9b0fa17798dc5c7649a370249d40f161
AugustoMagalhaes <augustommg@gmail.com>
2022-08-04 14:03:23 -0300

    feat: implementa usersRoute em app.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import productsRouter from './routes/products.route';
+import usersRouter from './routes/user.route';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productsRouter);
+
+app.use('/users', usersRouter);
 
 export default app;
